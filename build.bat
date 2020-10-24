5819750503fbaae9a2b71169592367133f4b5d55
vincentma0001 <vincent_ma0001@hotmail.com>
2020-10-24 20:38:19 +0800

    base version

---
+++ build.bat
@@ -0,0 +1,4 @@
+
+cmake e:/MyWork/vm_tools/ -B e:/MyWork/vm_tools/prj/cmake
+cmake --build e:/MyWork/vm_tools/prj/cmake
+
---
+++ vm_tools_ver.h
@@ -0,0 +1,11 @@
+#ifndef __VM_TOOLS_CFG_H_IN__
+#define __VM_TOOLS_CFG_H_IN__
+
+#define vName                   "vm_tools"
+#define vVersion                "1.0.0.0"
+#define vVerMajor               "1"
+#define vVerMinor               "0"
+#define vVerPatch               "0"
+#define vVerTweak               "0"
+
+#endif // __VM_TOOLS_CFG_H_IN__
