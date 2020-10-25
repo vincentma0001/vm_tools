e8e81cd5f488bdaa7fa49cd2a7e4a41ae43db46c
vm0001 <vm0001@localhost.localdomain>
2020-10-25 19:03:47 +0800

    add local ycm configure

---
+++ prj/cmake/tst/tst_cfgs/CMakeFiles/tst_cfgs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../../../bin/tst_cfgs"
+  "../../../../bin/tst_cfgs.pdb"
+  "CMakeFiles/tst_cfgs.dir/tst_cfgs.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tst_cfgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
