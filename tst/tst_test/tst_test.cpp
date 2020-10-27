e073edec4f07e8b154da517c91d28a4e281170c3
vm0001 <vm0001@localhost.localdomain>
2020-10-27 13:19:43 +0800

    add test code for ycm

--- tst/tst_test/tst_test.cpp
+++ tst/tst_test/tst_test.cpp
@@ -1,59 +1,31 @@
 
+
+#include <vector>
 #include <string>
-#include <iostream>
 #include <map>
+#include <iostream>
 
-// ================================================================================================ //
-// ==  Class CA : TODO add class brief here                                                      == //
-// ------------------------------------------------------------------------------------------------ //
-class CA
-// {{{
-{
-// ------------------------------------------------------------------------------------------------ //
-// Construct & Destruct : {{{
-public:
-    // Construct define
-    inline explicit CA();
-    // Destruct define
-    inline virtual ~CA();
-
-private:
-    // Copy construct define
-    inline CA             ( const CA &obj );
-    // Assignment operation
-    inline CA& operator = ( const CA &obj );
-// }}} ! Construct & Destruct
-
-// ------------------------------------------------------------------------------------------------ //
-// Menbers   : {{{
-private:
-public:
-    int miA;
-    int miB;
-    char* mszC;
-    /* TODO Add class's menbers here */
-// }}} ! Members
-
-// ------------------------------------------------------------------------------------------------ //
-// Methods   : {{{
-public:
-    char* toString(){return nullptr;};
-    int toCode(){return 10;};
-    /* TODO Add class's Methods here */
-// }}} ! Methods
-
-};
-// }}} ! [ class CA ]
+#include <tst_test/tst_test.h>
 
 int main(int argc, char *argv[])
 {
     std::cout << "This is a test!" << std::endl;
 
+    CA lA;
+    lA.toCode();
+
+    std::vector<int>    lV;
+    lV.push_back(19);
+    lV.push_back(20);
+
     std::string lstr="abced";
+    lstr.c_str();
     std::cout << lstr << std::endl;
-    
+
 
     std::map<int, std::string> lmap;
+    lmap.insert( std::pair<int, std::string>(10, "abcde"));
 
+//*/
     return 0;
 }
