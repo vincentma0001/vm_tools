f0c792561858a3818f0cd7898a8b3006fe1e2a68
vm0001 <vm0001@localhost.localdomain>
2020-10-28 09:14:13 +0800

    modify vm_cfg_def_sys.h

--- src/vm_cfgs/vm_cfg_def_sys.h
+++ src/vm_cfgs/vm_cfg_def_sys.h
@@ -7,7 +7,7 @@
 // ==   Author               : v.m. ( vincent_ma0001@hotmail.com )                               == //
 // ==   Version              : 0.0.0.0                                                           == //
 // ==   Create Time          : 2020-09-30 19:31:31                                               == //
-// ==   Modify Time          : 2020-10-28 09:12:49                                               == //
+// ==   Modify Time          : 2020-10-28 09:14:04                                               == //
 // ==   Issue  List          :                                                                   == //
 // ==   Change List          :                                                                   == //
 // ==     [    0.0.0.0     ] - Basic version                                                     == //
