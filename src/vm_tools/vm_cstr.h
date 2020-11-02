c4eb8fa673aba26a989724f512b9ecf470c5db66
vm0001 <vm0001@localhost.localdomain>
2020-11-02 23:24:39 +0800

    Modify vm_cstr.h

--- src/vm_tools/vm_cstr.h
+++ src/vm_tools/vm_cstr.h
@@ -7,7 +7,7 @@
 // ==   Author               : v.m. ( vincent_ma0001@hotmail.com )                               == //
 // ==   Version              : 1.0.0.0                                                           == //
 // ==   Create Time          : 2020-10-08 20:53:24                                               == //
-// ==   Modify Time          : 2020-11-02 12:18:01                                               == //
+// ==   Modify Time          : 2020-11-02 23:24:19                                               == //
 // ==   Issue  List          :                                                                   == //
 // ==   Change List          :                                                                   == //
 // ==     [    0.0.0.0     ] - Basic version                                                     == //
@@ -25,16 +25,6 @@
 // [ Include files ] {{{
 
 // std & crt function file included 
-#include <ctype.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-
-#include <wctype.h>
-#include <wchar.h>
 
 // vm's configure included
 #include <vm_cfgs.h>
